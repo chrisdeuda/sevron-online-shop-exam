f3a9e0e56efcd87323edee877aad44efa544c803
Chris Deuda <christopherdeuda@gmail.com>
2023-08-23 12:40:18 +0800

    chore(shopping-cart): temporary enable csrf on some endpoints

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,7 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        // TODO: Fix CSRF error token mismatch
         'login',
         'cart',
         'cart/*',
