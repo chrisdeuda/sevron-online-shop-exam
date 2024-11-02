2fdbc6a85dac9e4fc6af4d45e5302a7068e85063
Christopher Deuda <christopherdeuda@gmail.com>
2024-11-02 18:51:16 +0800

    feat: convert all dependencies to react

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,49 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: 'resources/js/app.js',
-            ssr: 'resources/js/ssr.js',
             refresh: true,
         }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
+        react(),
     ],
 });
+
+
+// import { defineConfig } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+// import react from '@vitejs/plugin-react';
+//
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: 'resources/js/app.js',
+//           //  ssr: 'resources/js/ssr.js',
+//             refresh: true,
+//         }),
+//         react(),
+//     ],
+//     esbuild: {
+//         loader: "jsx",
+//         include: /src\/.*\.jsx?$/,
+//         exclude: [],
+//     },
+//     optimizeDeps: {
+//         esbuildOptions: {
+//             plugins: [
+//                 {
+//                     name: "load-js-files-as-jsx",
+//                     setup(build) {
+//                         build.onLoad({ filter: /src\/.*\.js$/ }, async (args) => ({
+//                             loader: "jsx",
+//                             contents: await fs.readFile(args.path, "utf8"),
+//                         }));
+//                     },
+//                 },
+//             ],
+//         },
+//     },
+// });
