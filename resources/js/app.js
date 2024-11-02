7f49f192add6dac497a864d9becd81585a21dc8b
Christopher Deuda <chris@sevron.co.uk>
2024-11-02 18:51:16 +0800

    feat: convert all dependencies to react

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,29 +1,40 @@
 import './bootstrap';
 import '../css/app.css';
 
-import {createApp, h, ref} from 'vue';
-import { createInertiaApp } from '@inertiajs/vue3';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
-const cartTotal = ref(0);
-
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
-    setup({ el, App, props, plugin }) {
-        const app = createApp({ render: () => h(App, props) })
-            .use(plugin)
-            .use(ZiggyVue, Ziggy);
-
-        // Provide cartTotal for injection
-        app.provide('cartTotal', cartTotal);
-
-        return app.mount(el);
-    },
-    progress: {
-        color: '#4B5563',
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
+    setup({ el, App, props }) {
+        const root = createRoot(el);
+        root.render(React.createElement(App, props));
     },
 });
+
+
+//const cartTotal = ref(0);
+
+
+// createInertiaApp({
+//     title: (title) => `${title} - ${appName}`,
+//     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+//     setup({ el, App, props, plugin }) {
+//         const app = createApp({ render: () => h(App, props) })
+//             .use(plugin)
+//             .use(ZiggyVue, Ziggy);
+//
+//         // Provide cartTotal for injection
+//         app.provide('cartTotal', cartTotal);
+//
+//         return app.mount(el);
+//     },
+//     progress: {
+//         color: '#4B5563',
+//     },
+// });
